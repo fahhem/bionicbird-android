2b983358e568859b2aa2360b61a03e483c2e194f
Fahrzin Hemmati <fahhem@users.noreply.github.com>
2015-01-28 14:28:01 -0800

    Filter out repeated BLE addresses
    On Android 4.3, BLE scanning can return the same address if the RSSI level changes

--- www/js/main.js
+++ www/js/main.js
@@ -27,7 +27,7 @@ app.onDeviceReady = function()
 {
 	// The plugin was loaded asynchronously and can here be referenced.
 	ble = evothings.ble;
-	navigator.accelerometer.watchAcceleration(onSuccess, onError, {frequency: 80});
+	navigator.accelerometer.watchAcceleration(onAccel, errorCB, {frequency: 80});
   // For development only.
   // toggle_scan(true);
 };
@@ -38,7 +38,7 @@ function resetValues() {
 	update();
 }
 
-function onSuccess(acceleration) {
+function onAccel(acceleration) {
 	if(!started) {
 		currentAccYValue = -acceleration.y;
 	} else {
@@ -73,27 +73,8 @@ function onSuccess(acceleration) {
 	}
 };
 
-function onError() {
-    console.log('onError!');
-};
-
 var errorCB = function(err) {
   console.log('error: ' + JSON.prune(err));
-};
-
-var got_device = function(r) {
-  console.log('got device ' + r.address);
-  $('#deviceList').append(
-      '<li><button class="deviceBtn" data-addr="' + r.address + '">'
-      + r.address + ' ' + r.rssi + ' ' + r.name + '</button></li>');
-  $('.deviceBtn').click(function() {
-    toggle_scan(false);
-    $('.deviceBtn').prop('disable', true);
-    ble.connect(this.dataset.addr, connected, errorCB);
-  });
-  // if (r.address == '54:4A:16:54:EE:F0') {
-  //   $('.deviceBtn').last().click();
-  // }
 };
 
 var connected = function(info) {
@@ -149,21 +130,10 @@ var got_service_data = function(deviceHandle, services) {
     console.log('read thruster: ' + JSON.prune(data));
     $("#thrust").val(data[0] || 0);
   });
-  
-  // read_value('battery', function(data) {
-  //   console.log('read battery ' + JSON.prune(data));
-  // });
-  // subscribe to battery changes
-  // ble.enableNotification(app.device_handle, app.bird_chrs.battery.handle,
-  //     function(data) {
-  //       console.log('battery ' + JSON.prune(ble.fromUtf8(data)));
-  //     }, errorCB);
   $('#controls').show();
 };
 
 var write_value = function(type, data) {
-  // console.log('writing... ' + JSON.prune(app.bird_chrs[type].uuid)
-  //     + ' <- ' + JSON.prune(data));
   ble.writeCharacteristic(
       app.device_handle, app.bird_chrs[type].handle,
       data, null, errorCB);
@@ -176,14 +146,33 @@ var read_value = function(type, cb) {
 };
 
 var clear_devices = function() {
-  $("#deviceList").children()./*nextAll().*/remove();
+  $("#deviceList").children().remove();
   $('#devices').show();
   $('#controls').hide();
 };
 
+var got_device = function(r) {
+  console.log('got device ' + r.address);
+  if (app.seen_devices.indexOf(r.address) != -1) {
+    // Already seen this device.
+    return;
+  }
+  app.seen_devices.push(r.address);
+  $('#deviceList').append(
+      '<li><button class="deviceBtn" data-addr="' + r.address + '">'
+      + r.address + ' ' + r.rssi + ' ' + r.name + '</button></li>');
+  $('.deviceBtn').click(function() {
+    toggle_scan(false);
+    $('.deviceBtn').prop('disable', true);
+    ble.connect(this.dataset.addr, connected, errorCB);
+  });
+};
+
 app.scanning = false;
+app.seen_devices = [];
 var toggle_scan = function(scanning) {
   app.scanning = scanning;
+  app.seen_devices = [];
   var scanBtn = $('#scan')[0];
   scanBtn.innerText = scanBtn.dataset[scanning];
   if (scanning) {
